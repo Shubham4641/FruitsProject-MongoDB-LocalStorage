4d23598777bfcb2fdbeac86deb0043a9ce51f908
shubham4641 <shubham.sri4641@gmail.com>
2021-05-17 21:10:16 +0530

    First Commit

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.post("/failure", function(req, res)
     res.redirect("/");
 })
 
-app.listen(process.env.PORT || 3000, function()
+app.listen(process.env.PORT|| 3000, function()
 {
     console.log("server started on port 3000");
 });
